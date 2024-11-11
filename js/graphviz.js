7ccd7dd904013f31840e0d21f5ee2cbdc41150aa
Tony Hirst <tony.hirst@gmail.com>
2024-11-11 12:50:19 +0000

    refactor how we load graphviz wasm

--- js/graphviz.js
+++ js/graphviz.js
@@ -8,38 +8,36 @@ import { Graphviz } from "@hpcc-js/wasm-graphviz";
 
 function render({ model, el }) {
   const _headless = model.get("headless");
-  if (!_headless) {
-    let el2 = document.createElement("div");
-    el2.innerHTML = html;
-    const uuid = generateUUID();
-    el2.id = uuid;
-    el.appendChild(el2);
-  }
-
-  model.on("change:code_content", () => {
-    const dot = model.get("code_content");
-
-    function handle_svg(model, graphviz) {
-      const svg = graphviz.dot(dot);
-      model.set("svg", svg);
-      model.save_changes();
-      return svg;
+  Graphviz.load().then((graphviz) => {
+    if (!_headless) {
+      let el2 = document.createElement("div");
+      el2.innerHTML = html;
+      const uuid = generateUUID();
+      el2.id = uuid;
+      el.appendChild(el2);
     }
 
-    if (!_headless) {
-      const img_el = el.querySelector('div[title="image-container"]');
-      const dot_el = el.querySelector('pre[title="dot-container"]');
-      dot_el.innerHTML = dot;
-      img_el.innerHTML = "Processing...";
-      Graphviz.load().then((graphviz) => {
+    model.on("change:code_content", () => {
+      const dot = model.get("code_content");
+
+      function handle_svg(model, graphviz) {
+        const svg = graphviz.dot(dot);
+        model.set("svg", svg);
+        model.save_changes();
+        return svg;
+      }
+
+      if (!_headless) {
+        const img_el = el.querySelector('div[title="image-container"]');
+        const dot_el = el.querySelector('pre[title="dot-container"]');
+        dot_el.innerHTML = dot;
+        img_el.innerHTML = "Processing...";
         const svg = handle_svg(model, graphviz);
         img_el.innerHTML = svg;
-      });
-    } else {
-       Graphviz.load().then((graphviz) => {
-         handle_svg(model, graphviz);
-       });
-    }
+      } else {
+        handle_svg(model, graphviz);
+      }
+    });
   });
 }
 
